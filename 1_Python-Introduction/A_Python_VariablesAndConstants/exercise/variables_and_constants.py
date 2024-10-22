21d9405eaa8eb59eb5b0bd50e7920cc9be51f949
Joe Bracey <joebracey1@gmail.com>
2024-10-22 19:02:09 +0100

    Exercise 1 complete

---
+++ 1_Python-Introduction/A_Python_VariablesAndConstants/exercise/variables_and_constants.py
@@ -0,0 +1,17 @@
+# Define variables
+age = 27
+name = "Joe"
+true_or_false = True
+floaty = 3.14
+# Define constants
+PI = 3.14159
+COMPUTER = "Mac"
+# Do some printing
+print("Age = ", age)
+print("Name = ", name)
+print(true_or_false)
+print(floaty)
+print(PI)
+print(COMPUTER)
+# Below will not print as COMPUTER is defined not computer
+print(computer)
